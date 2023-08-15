16d63d556cc3fc51ad6d89d95af75415a0a7759e
RefinedSnack <RefinedSnack@gmail.com>
2023-08-15 13:41:12 -0600

    added agent_state enum

---
+++ PythontoCpptest/PythontoCpptest/Direction.h
@@ -0,0 +1,16 @@
+//
+// Created by walte on 8/15/2023.
+//
+
+#ifndef RESEARCH_LAB_DIRECTION_H
+#define RESEARCH_LAB_DIRECTION_H
+
+
+
+class Direction {
+
+};
+
+
+
+#endif //RESEARCH_LAB_DIRECTION_H
---
+++ PythontoCpptest/PythontoCpptest/Position.h
@@ -0,0 +1,16 @@
+//
+// Created by walte on 8/15/2023.
+//
+
+#ifndef RESEARCH_LAB_POSITION_H
+#define RESEARCH_LAB_POSITION_H
+
+
+
+class Position {
+
+};
+
+
+
+#endif //RESEARCH_LAB_POSITION_H
